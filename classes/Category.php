5765c7a4fbde9346e69269a5aefb8292dce08034
titoValen <valentin.tito@davinci.edu.ar>
2026-06-02 15:43:22 -0300

    Añadir clase Category con getters y setters para id y name

---
+++ classes/Category.php
@@ -0,0 +1,23 @@
+<?php
+class Category
+{
+  private $id;
+  private $name;
+
+  // Getters
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  // Setters
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+}
