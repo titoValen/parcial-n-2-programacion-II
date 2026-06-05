83769f66e2336981a7ba994d306cbc64fa2afc95
titoValen <valentin.tito@davinci.edu.ar>
2026-06-05 17:52:54 -0300

    Refactor video selection logic in hero.js and update video source assignment

--- js/hero.js
+++ js/hero.js
@@ -1,4 +1,13 @@
-import { padre_hero } from "./elements.js";
+import { $ } from "./elements.js";
 
 const path = "./video/";
 const videos = ["video_1.mp4", "video_2.mp4", "video_3.mp4", "video_4.mp4"];
+const element_video = $.querySelector(".home-presentacion video");
+
+function changeVideo() {
+  const randomIndex = Math.floor(Math.random() * videos.length);
+  const selectedVideo = videos[randomIndex];
+  element_video.src = path + selectedVideo;
+}
+
+changeVideo();
