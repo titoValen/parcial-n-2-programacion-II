351014737570af6e2ef259836b74b209eab78b30
titoValen <valentin.tito@davinci.edu.ar>
2026-06-08 16:01:53 -0300

    Agregar comentarios para mejorar la legibilidad del código en filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,6 @@
 import { $ } from "./elements.js";
 
+// Funcionalidad para mostrar/ocultar el filtro
 $.addEventListener("DOMContentLoaded", () => {
   const filterButton = $.querySelector(".filter-button");
   const filterAside = $.querySelector("aside");
@@ -13,10 +14,35 @@ $.addEventListener("DOMContentLoaded", () => {
     filterAside.classList.toggle("active");
   });
 
-  // Cerrar el filtro al hacer click fuera
   $.addEventListener("click", (e) => {
     if (!e.target.closest(".filter-button") && !e.target.closest("aside")) {
       filterAside.classList.remove("active");
     }
+  });
+});
+
+// Funcionalidad para seleccionar categorías y marcas
+$.addEventListener("DOMContentLoaded", () => {
+  const listItemsCategory = $.querySelectorAll(".filter-item-category");
+  const listItemsBrand = $.querySelectorAll(".filter-item-brand");
+
+  listItemsCategory.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (item.classList.contains("active")) {
+        item.classList.remove("active");
+      } else {
+        item.classList.toggle("active");
+      }
+    });
+  });
+
+  listItemsBrand.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (item.classList.contains("active")) {
+        item.classList.remove("active");
+      } else {
+        item.classList.toggle("active");
+      }
+    });
   });
 });
