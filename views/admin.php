8cfa587d9cff566796930aecb20bda60ab09f2b8
titoValen <valentin.tito@davinci.edu.ar>
2026-06-16 15:45:44 -0300

    Actualizar la ruta del formulario en admin.php para corregir el procesamiento de datos

--- views/admin.php
+++ views/admin.php
@@ -1,7 +1,7 @@
 <main>
   <h1>Administración</h1>
 
-  <form action="./process/process_admin.php" method="post">
+  <form action="./process/form_admin.php" method="post">
     <div>
       <label for="name">Nombre</label>
       <input required type="text" id="name" name="name">
