d72561f5927abbc1f14feb413db66482a889556c
titoValen <valentin.tito@davinci.edu.ar>
2026-06-23 15:11:15 -0300

    Agregar método productById en la clase Product para obtener un producto por su ID

--- classes/Product.php
+++ classes/Product.php
@@ -74,4 +74,33 @@ class Product
 
     return $PDOStatement->fetchAll();
   }
+
+  public static function productById($id)
+  {
+    $PDO = (new DB())->getDB();
+
+    $query = "
+      SELECT
+        p.id,
+        p.name,
+        p.description,
+        p.price,
+        p.image,
+        p.alt,
+        p.stock,
+        c.name AS category,
+        b.name AS brand
+      FROM product p
+      INNER JOIN category c ON p.id_category = c.id
+      INNER JOIN brand    b ON p.id_brand    = b.id
+      WHERE p.id = :id
+    ";
+
+    $PDOStatement = $PDO->prepare($query);
+    $PDOStatement->bindParam(':id', $id, PDO::PARAM_INT);
+    $PDOStatement->setFetchMode(PDO::FETCH_CLASS, self::class);
+    $PDOStatement->execute();
+
+    return $PDOStatement->fetch();
+  }
 }
