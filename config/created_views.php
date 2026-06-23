156ed359dd1d32bd9588992ed994ccae34528cc6
titoValen <valentin.tito@davinci.edu.ar>
2026-06-23 15:30:32 -0300

    Agregar 'res_form' a la lista de vistas creadas en created_views.php

--- config/created_views.php
+++ config/created_views.php
@@ -1,2 +1,2 @@
 <?php
-  $vistas_creadas = ['home', 'producto', 'detalle', 'contacto', 'alumno', 'admin'];
+  $vistas_creadas = ['home', 'producto', 'detalle', 'contacto', 'alumno', 'admin', 'res_form'];
