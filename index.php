5377f38c6eee6d03a9d603f0af0449e15244bbbb
titoValen <valentin.tito@davinci.edu.ar>
2026-06-28 02:23:15 -0300

    Reorganizar la carga de vistas y mejorar la gestión de la sesión en index.php

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
-<?php 
-require_once 'components/head.php'; 
+<?php
+session_start();
+
+require_once 'config/created_views.php';
+
+if (
+  isset($_GET['vista']) &&
+  !empty($_GET['vista']) &&
+  in_array($_GET['vista'], $vistas_creadas)
+) {
+  $vista = $_GET['vista'];
+} else {
+  $vista = 'home';
+}
+
+if ($vista === 'admin' && (!isset($_SESSION['user']) || ($_SESSION['user']['role'] ?? null) !== 'admin')) {
+  $vista = 'form_admin';
+}
+
+$_GET['vista'] = $vista;
+
+require_once 'components/head.php';
 ?>
 
 <body>
   <?php require_once 'components/header.php'; ?>
-  <?php
-  require_once 'config/created_views.php';
-  if (
-    isset($_GET['vista']) &&
-    !empty($_GET['vista']) &&
-    in_array($_GET['vista'], $vistas_creadas)
-  ) {
-    $vista = $_GET['vista'];
-  } else {
-    $vista = 'home';
-  }
-
-  require_once "views/$vista.php";
-
-  ?>
+  <?php require_once "views/$vista.php"; ?>
   <?php require_once 'components/footer.php'; ?>
 </body>
 
